4ce407bf375f36010af6907ed8e9e0966c16a365
luismsantanaa <luismsantana@gmail.com>
2020-04-20 20:24:26 -0400

    Added Modules to Siplify structured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
+import { PagesModule } from './pages/pages.module';
+
 // Routes
-import { APP_ROUTES } from "./app.routes";
+import { APP_ROUTES } from './app.routes';
+
 // Components
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafic1Component } from './pages/grafic1/grafic1.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { RegisterComponent } from './login/register.component';
-import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PagenotfoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Grafic1Component,
-    NavbarComponent,
-    SidebarComponent,
-    HeaderComponent,
-    BreadcrumbsComponent,
-    RegisterComponent,
-    PagesComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafic1Component } from './pages/grafic1/grafic1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
 import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 import { RegisterComponent } from './login/register.component';
 import { LoginComponent } from './login/login.component';
 
 const ROUTES: Routes = [
-        {
-            path: '',
-            component: PagesComponent,
-            children: [
-                { path: 'dashboard', component: DashboardComponent },
-                { path: 'grafics', component: Grafic1Component },
-                { path: 'progress', component: ProgressComponent },
-                { path: '',  pathMatch: 'full', redirectTo: '/dashboard' }
-            ]
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: PagenotfoundComponent }
